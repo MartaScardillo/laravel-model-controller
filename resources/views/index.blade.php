3978378c00dbefec5db226f5366523ea01eecaaa
Marta Scardillo <martascardillo02@gmail.com>
2023-12-12 17:25:03 +0100

    Add cards style

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
 </head>
 <body>
     <section>
